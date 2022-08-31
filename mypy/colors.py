e11855f7af65183ab27cddd7560a525bf7c0640d
sempervent <jngrant@gmail.com>
2022-08-31 00:21:14 -0400

    more colors

--- mypy/colors.py
+++ mypy/colors.py
@@ -33,10 +33,95 @@ class Colors:
     LIGHT_MAGENTA = "\033[95m"
     LIGHT_CYAN = "\033[96m"
 
-    def print(message: str,
-              preceeding_color: str = '',
-              proceeding_color: str = '',
-              ):
-        """Print message to stdout."""
-        print(preceeding_color + f"{message}" + proceeding_color)
+    def lighht_yellow(self):
+        self.sequences.append(Colors.LIGHT_YELLOW)
+        self.ending.append(Colors.RESET)
+        return self
 
+    def light_green(self):
+        self.sequences.append(Colors.LIGHT_GREEN)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def dark_gray(self):
+        self.sequences.append(Colors.DARK_GRAY)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def light_red(self):
+        self.sequences.append(Colors.LIGHT_RED)
+        self.ending.apend(Colors.RESET)
+        return self
+
+    def light_gray(self):
+        self.sequences.append(Colors.LIGHT_GRAY)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def cyan(self):
+        self.sequences.append(Colors.CYAN)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def yellow(self):
+        self.sequences.append(Colors.YELLOW)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def blue(self):
+        self.sequences.append(Colors.BLUE)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def magenta(self):
+        self.sequences.append(Colors.MAGENTA)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def green(self):
+        self.sequences.append(Colors.GREEN)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def red(self):
+        self.sequences.append(Colors.RED)
+        self.ending.append(Colors.RESET)
+        return self
+
+    def __init__(self, text: str = ""):
+        self.text = text
+        self.ending = []
+        self.sequences = []
+
+    def bold(self):
+        self.sequences.append(Colors.BOLD)
+        self.ending.append(Colors.RESET_BOLD)
+        return self
+
+    def underline(self):
+        self.sequences.append(Colors.UNDERLINE)
+        self.endings.append(Colors.RESET_UNDERLINE)
+        return self
+
+    def dim(self):
+        self.sequences.append(Colors.DIM)
+        self.ending.append(Colors.RESET_DIM)
+        return self
+
+    def blink(self):
+        self.sequences.append(Colors.BLINK)
+        self.ending.append(Colors.RESET_BLINK)
+        return self
+
+    def hidden(self):
+        self.sequences.append(Colors.HIDDEN)
+        self.ending.append(Colors.REST)
+        return self
+
+    def __str__(self):
+        return ''.join(self.sequences) + self.text + ''.join(self.ending)
+
+    def black(self):
+        self.sequences.append(Colors.BLACK)
+        self.ending.append(Colors.RESET)
+        return self
