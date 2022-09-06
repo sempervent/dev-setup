337b9c195900024a6956f3fcd71b09b124806c3c
sempervent <jngrant9@gmail.com>
2022-09-06 13:32:35 -0400

    use VERSION file for versioning

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ import pathlib
 from setuptools import setup
 
 _SCRIPTS = [str(pathlib.Path('bin/') / script) for script in os.listdir('bin')]
+_VERSION = pathlib.Path('VERSION').read_text(encoding='UTF-8').splitlines()[0]
 
 setup(
     name="mypy",
-    version="0.0.2",
+    version=_VERSION,
     description="Common everyday tools made easier.",
     author="Joshua N. Grant",
     author_email="jngrant@live.com",
