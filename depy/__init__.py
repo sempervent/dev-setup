2ae1cd9723740d9abb6f40ad9c3dba3e39443788
sempervent <jngrant9@gmail.com>
2022-09-07 15:47:28 -0400

    change unlist to yield a generator object

--- depy/__init__.py
+++ depy/__init__.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Allow imports from this directory."""
-import itertools
-from collections.abc import Iterable
-from typing import Iterable as IterableType
+from typing import Iterable
+
+PRIMITIVES = (str, bytes, bool, float, int)
 
 
-def unlist(lst: IterableType):
+def unlist(lst: Iterable):
     """Unlist an iterable."""
-    if isinstance(lst, Iterable):
-        return list(itertools.chain.from_iterable(lst))
-    return lst
+    for item in lst:
+        if isinstance(item, Iterable) and not isinstance(item, PRIMITIVES):
+            yield from unlist(item)
+        else:
+            yield item
 
 
 if __name__ == "__main__":
-    test_list = [[1, 2], [3, 4, 5], [6, 7], [8, [9]]]
-    print(unlist(test_list))
+    test_list = [[1, 2], [3, 4, 5], [6, 7], [8, [9]], 10]
+    print(list(unlist(test_list)))
 
